27969fd1cb39092f4e3c888433aeae30f73e0471
Alexander Gomez Higuita <agomez@enertotalesp.com>
2019-11-22 17:23:32 -0500

    Create function for fix the length of both numbers

--- DivAndConq/karatsuba_mult.py
+++ DivAndConq/karatsuba_mult.py
@@ -1,12 +1,67 @@
 import math
+import functools
+import operator
 
 
-def karatsuba_mult(number_01, number_02):
-    number_01_digits = len(str(number_01))
-    number_02_digits = len(str(number_02))
-    if math.log(number_01_digits, 2) != 0:
-        raise Exception("Your number is not power of 2")
-    if number_01_digits != number_02_digits:
-        raise Exception("Both numbers must have the same number of digits")
+def fix_number_length(x_fix: str, y_fix: str):
+    """
+    Fix the length of two number
 
-    return None
+    For the Karatsuba multiplication both numbers must be the same digits and the length of both numbers must be power
+    of 2. For example, if the number is '987' then the length of this number is 3 and 3 is not power of 2, '987' must be
+    transform to '0987' because 4 is power of 2. The number '89872' must be transform to '00089872' and so on.
+
+    :param x_fix: number 01
+    :param y_fix: number 02
+    :return: tuple of fixed numbers
+    """
+    # Find the max length of both numbers
+    max_length = len(x_fix) if len(x_fix) >= len(y_fix) else len(y_fix)
+    power_2_number = 0
+
+    # Find the power of two length
+    log_operation = math.log(max_length, 2)
+    if log_operation % 1 != 0:
+        power_2_number = (log_operation // 1) + 1
+    else:
+        power_2_number = log_operation
+
+    number_digits = 2**power_2_number
+
+    # Fix x
+    if len(x_fix) < number_digits:
+        zero_str = '0' * int(number_digits - len(x_fix))
+        x_fix = zero_str + x_fix
+
+    # Fix y
+    if len(y_fix) < number_digits:
+        zero_str = '0' * int(number_digits - len(y_fix))
+        y_fix = zero_str + y_fix
+
+    return x_fix, y_fix
+
+
+def karatsuba_mult(x_str: str, y_str: str):
+    """
+    Karatsuba multiplication
+
+
+    :param x: number 01
+    :param y: number 02
+    :return: number 01 * number 02
+    """
+    number_digits = len(x_str)
+
+    # Base case, both numbers have 1 digit
+    if len(x_str) == len(y_str) == 1:
+        return functools.reduce(operator.add, [int(x)]*int(y))
+    else:
+        None
+
+
+if __name__ == '__main__':
+    x = 9
+    y = 32
+    x, y = fix_number_length(str(x), str(y))
+    result = karatsuba_mult(x, y)
+    print('x[{}] * y[{}] = {}'.format(x, y, result))
--- test/test_karatsuba_mult.py
+++ test/test_karatsuba_mult.py
@@ -1,35 +1,22 @@
 import pytest
-from DivAndConq.karatsuba_mult import karatsuba_mult
+from DivAndConq.karatsuba_mult import karatsuba_mult, fix_number_length
 
 
-def test_2d_karatsuba_mult():
-    number_01 = 92
-    number_02 = 81
-    assert karatsuba_mult(number_01, number_02) == 7452
+def test_01_karatsuba_mult():
+    x = '900'
+    y = '1'
+    assert fix_number_length(x, y) == ('0900', '0001')
+    assert fix_number_length(y, x) == ('0001', '0900')
 
 
-def test_4d_karatsuba_mult():
-    number_01 = 9276
-    number_02 = 8312
-    assert karatsuba_mult(number_01, number_02) == 77102112
+def test_02_karatsuba_mult():
+    x = '9877'
+    y = '1'
+    assert fix_number_length(x, y) == ('9877', '0001')
 
 
-def test_8d_karatsuba_mult():
-    number_01 = 92764576
-    number_02 = 81431254
-    assert karatsuba_mult(number_01, number_02) ==   7.553935750458300
-
-
-def test_err01_karatsuba_mult():
-    number_01 = 9276457
-    number_02 = 8143125
-    with pytest.raises(Exception):
-        karatsuba_mult(number_01, number_02)
-
-
-def test_err02_karatsuba_mult():
-    number_01 = 92764578
-    number_02 = 8143
-    with pytest.raises(Exception):
-        karatsuba_mult(number_01, number_02)
+def test_03_karatsuba_mult():
+    x = '98771'
+    y = '187652'
+    assert fix_number_length(x, y) == ('00098771', '00187652')
 
