e812527da46c4781285650e38c3902ab84165695
Alexander Gomez Higuita <agomez@enertotalesp.com>
2019-12-02 22:52:12 -0500

    Recursive approach

--- DivAndConq/counting_invertions.py
+++ DivAndConq/counting_invertions.py
@@ -16,15 +16,61 @@ def brute_force(numbers: list) -> int:
     return count
 
 
-def recursive_aproach(numbers: list) -> int:
-    return 0
+def count_split_inver(numbers_l: list, numbers_r: list) -> (int, list):
+    """
+    Count split inversion
+
+    :param numbers_l: ordered list a
+    :param numbers_r: ordered list b
+    :return: count of inversions, ordered list (a + b)
+    """
+    result = []
+    count = 0
+    i = 0
+    j = 0
+    while i < len(numbers_l) and j < len(numbers_r):
+        if numbers_l[i] <= numbers_r[j]:
+            result.append(numbers_l[i])
+            i += 1
+            count += j
+        else:
+            result.append(numbers_r[j])
+            j += 1
+
+    if i >= len(numbers_l):
+        result += numbers_r[j:]
+    else:
+        result += numbers_l[i:]
+        count += len(numbers_l[i:]) * j
+
+    return count, result
+
+
+def recursive_approach(numbers: list) -> (int, list):
+    """
+    recursive approach of counting inversions
+
+    This functions split in two sub-list, and for each sub-list et the count and the ordered list, and finally invoque
+    the count_split_inver to count inversions in the union of both ordered sub-list.
+    :param numbers: list of numbers
+    :return: tuple with: counting invertions, ordered list
+    """
+
+    if len(numbers) <= 1:
+        return 0, numbers
+
+    mid = len(numbers) // 2
+    count_a, left_numbers = recursive_approach(numbers[:mid])
+    count_b, right_numbers = recursive_approach(numbers[mid:])
+    count, ordered = count_split_inver(left_numbers, right_numbers)
+    count += count_a + count_b
+    return count, ordered
 
 
 if __name__ == '__main__':
     with open('IntegerArray.txt') as file:
         numbers = file.read().rstrip('\n').split('\n')
+    numbers = [int(x) for x in numbers]
 
-    n = brute_force(numbers[:100])
-
-    print(f'Result, n:{n}')
-
+    c, ordered = recursive_approach(numbers)
+    print(f'Result, inversions:{c} [:15]={ordered[:15]}, [-15:]:{ordered[-15:]}')
--- test/test_counting_invertions.py
+++ test/test_counting_invertions.py
@@ -1,4 +1,4 @@
-from DivAndConq.counting_invertions import brute_force, recursive_aproach
+from DivAndConq.counting_invertions import brute_force, recursive_approach
 
 
 def test_bf_001():
@@ -9,5 +9,20 @@ def test_bf_001():
 
 def test_ra_001():
     numbers = [1, 3, 5, 2, 4, 6]
-    counting_invertions = recursive_aproach(numbers)
-    assert counting_invertions == 3
+    counting_invertions, numbers_ordered = recursive_approach(numbers)
+    assert counting_invertions == 3 and numbers_ordered == [1, 2, 3, 4, 5, 6]
+
+
+def test_ra_002():
+    numbers = [5, 7, 8, 9, 10, 20, 26, 0, 1, 2, 3, 6, 21, 25]
+    counting_invertions, numbers_ordered = recursive_approach(numbers)
+    assert counting_invertions == 36 and numbers_ordered == [0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 20, 21, 25, 26]
+
+
+def test_ra_003():
+    with open('DivAndConq/IntegerArray.txt') as file:
+        numbers = file.read().rstrip('\n').split('\n')
+    numbers = [int(x) for x in numbers]
+
+    c, ordered = recursive_approach(numbers)
+    assert c == 2407905288
